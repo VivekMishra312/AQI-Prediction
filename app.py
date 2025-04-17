283c58b1504175491b37f2874d3008621e1e74d0
VivekMishra312 <137587112+VivekMishra312@users.noreply.github.com>
2025-04-17 18:42:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
